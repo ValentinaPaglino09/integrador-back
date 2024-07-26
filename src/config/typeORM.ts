01e197195e5ae9200adb5baf865c02e2b599b643
Valentina Paglino <capacitaciones06.09@adviters.com>
2024-07-26 17:32:23 -0300

    changes

--- src/config/typeORM.ts
+++ src/config/typeORM.ts
@@ -3,11 +3,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeORM = (): TypeOrmModuleOptions => {
   return {
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'admin1234',
-    database: 'integrador',
+    host: '',
+    port: 1,
+    username: '',
+    password: '',
+    database: '',
     entities: ['dist/**/*.entity.{ts,js}'],
     synchronize: true,
     // logging: true,
